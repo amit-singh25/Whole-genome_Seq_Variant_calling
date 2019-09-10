4f3e30447f20e59c6b9435b3b33e128c507d682a
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2019-09-10 13:56:19 +0200

    Update Variant_anno.sh

--- Variant Annotation/Variant_anno.sh
+++ Variant Annotation/Variant_anno.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+#PBS -N Variant_anno
+##PBS -j oe
+#PBS -m e
+##PBS -l file=100GB
+#PBS-l mem=100G
+#PBS -l walltime=50:13:59
+#PBS -l nodes=1:ppn=8   
+#PBS -e ./Variant_anno_$PBS_JOBID.err           # stderr file
+#PBS -o ./Variant_anno_$PBS_JOBID.out
+#PBS -V
+#echo $PBS_JOBNAME
+#echo $PBS_JOBID
 
+name=$1
+gtf=~/genome
+data=~/alignment
+out=~/Variant_files
+
+vep --cache -i ${out}/${name}.vcf.gz --output_file ${out}/${name}_variant_ann.txt
