8d00b7c503fb3af327a35729cc5e9c3df18bf91b
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2019-09-10 13:37:12 +0200

    Update Variant_calling.sh

--- Variant calling/Variant_calling.sh
+++ Variant calling/Variant_calling.sh
@@ -1,1 +1,10 @@
 
+
+
+#Indexing genome fasta 
+samtools faidx hs37d5.fa
+bcftools mpileup -O b -o file1.chr19.bwa_raw.bcf -f hs37d5.fa file1.chr19.bwa.bam
+bcftools call -m -v -o file1.chr19.bwa_raw.vcf -O v file1.chr19.bwa_raw.bcf
+vcfutils.pl varFilter file1.chr19.bwa_raw.vcf >file1.chr19.bwa_final_variant.vcf
+bgzip file1.chr19.bwa_final_variant.vcf
+tabix -p vcf file1.chr19.bwa_final_variant.vcf.gz
