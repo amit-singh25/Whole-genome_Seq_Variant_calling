760766cd5a89fc00261479b20d3610bd038419fd
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2019-09-10 13:53:12 +0200

    Update Variant_calling.sh

--- Variant calling/Variant_calling.sh
+++ Variant calling/Variant_calling.sh
@@ -1,10 +1,29 @@
+#!/bin/bash
+#PBS -N Variant_calling
+##PBS -j oe
+#PBS -m e
+##PBS -l file=100GB
+#PBS-l mem=100G
+#PBS -l walltime=50:13:59
+#PBS -l nodes=1:ppn=8   
+#PBS -e ./Variant_calling_$PBS_JOBID.err           # stderr file
+#PBS -o ./Variant_calling_$PBS_JOBID.out
+#PBS -V
+#echo $PBS_JOBNAME
+#echo $PBS_JOBID
 
+name=$1
+gtf=~/genome
+data=~/alignment
+out=~/Variant_files
 
 
 #Indexing genome fasta 
 samtools faidx hs37d5.fa
-bcftools mpileup -O b -o file1.chr19.bwa_raw.bcf -f hs37d5.fa file1.chr19.bwa.bam
-bcftools call -m -v -o file1.chr19.bwa_raw.vcf -O v file1.chr19.bwa_raw.bcf
-vcfutils.pl varFilter file1.chr19.bwa_raw.vcf >file1.chr19.bwa_final_variant.vcf
-bgzip file1.chr19.bwa_final_variant.vcf
-tabix -p vcf file1.chr19.bwa_final_variant.vcf.gz
+bcftools mpileup -O b -o ${out}/${name}.bwa_raw.bcf -f ${gtf}/hs37d5.fa ${data}/${name}.bwa.bam
+bcftools call -m -v -o ${out}/${name}.bwa_raw.vcf -O v ${out}/${name}.bwa_raw.bcf
+vcfutils.pl varFilter ${out}/${name}.bwa_raw.vcf >${out}/${name}.bwa_final_variant.vcf
+bgzip ${out}/${name}.bwa_final_variant.vcf
+tabix -p vcf ${out}/${name}.bwa_final_variant.vcf.gz
+
+
